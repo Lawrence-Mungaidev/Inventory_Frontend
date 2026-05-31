b6274e1b57a146468bd56317a74ec7d650c8840b
Lawrence_Mungai <mungailawrence52@gmail.com>
2026-05-31 13:24:49 +0300

    feat: add QS favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -86,11 +86,15 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:card", content: "summary" },
     ],
     links: [
-      {
-        rel: "stylesheet",
-        href: appCss,
-      },
-    ],
+  {
+    rel: "stylesheet",
+    href: appCss,
+  },
+  { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+  { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+  { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  ],
   }),
   shellComponent: RootShell,
   component: RootComponent,
