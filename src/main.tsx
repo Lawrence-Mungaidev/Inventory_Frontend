708ce5d1ad9eae9109f8d9139037e3abf9fec0a3
Lawrence_Mungai <mungailawrence52@gmail.com>
2026-06-01 17:42:08 +0300

    fix: working SPA setup with stripped root route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { getRouter } from "./router";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { routeTree } from "./routeTree.gen";
 import "./styles.css";
 
 const queryClient = new QueryClient();
-const router = getRouter();
+
+const router = createRouter({
+  routeTree,
+  context: { queryClient },
+  defaultPreloadStaleTime: 0,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
